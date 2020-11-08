a2ccbb1aaa8bf6e30015df7739f1ee9ae9368611
yahiathen <yahiathen@gmail.com>
2020-11-08 15:04:18 +0100

    init project organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The task is to forecast the total amount of products sold in every shop',
+    author='yahiathen',
+    license='',
+)
